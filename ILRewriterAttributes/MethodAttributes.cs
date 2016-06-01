fbe807f682778265ed14c1323e2d4dc5fdd07c48
Markus Schweitzer <me@markusschweitzer.at>
2016-06-02 00:47:20 +0200

    Added attributes library

---
+++ ILRewriterAttributes/MethodAttributes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILRewriterAttributes
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public class MethodLogging : Attribute
+    {
+        public static void PreMethod(string name)
+        {
+            Console.WriteLine(string.Format("{0} Enter method: '{1}", DateTime.Now, name));
+        }
+        public static void PostMethod(string name)
+        {
+            Console.WriteLine(string.Format("{0} Leaving method: '{1}", DateTime.Now, name));
+        }
+    }
+}
