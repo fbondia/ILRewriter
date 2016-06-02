334cb97cd1800734490394a98e1df301c1601322
Markus Schweitzer <me@markusschweitzer.at>
2016-06-02 23:04:08 +0200

    Added NotNullOrEmpty parameter attribute.

--- ILRewriterAttributes/ParameterAttributes.cs
+++ ILRewriterAttributes/ParameterAttributes.cs
@@ -11,10 +11,28 @@ namespace ILRewriterAttributes
     {
         public static void Process(string methodName, string name, object value)
         {
-            if(value == null)
+            if (value == null)
             {
-                throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' is null.",methodName, name));
+                throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' is null.", methodName, name));
             }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public class NotNullOrEmpty : Attribute
+    {
+        public static void Process(string methodName, string name, object value)
+        {
+            if(value != null)
+            {
+                string data = value.ToString();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    return;
+                }
+            }
+
+            throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' is null or empty.", methodName, name));
         }
     }
 }
