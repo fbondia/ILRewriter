36260046596d9c679292ad7c897c58feb713e769
Markus Schweitzer <me@markusschweitzer.at>
2016-06-02 21:17:23 +0200

    Added duration to MethodLogging

--- ILRewriterAttributes/MethodAttributes.cs
+++ ILRewriterAttributes/MethodAttributes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,18 @@ namespace ILRewriterAttributes
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
     public class MethodLogging : Attribute
     {
+        Stopwatch stopWatch;
+
         public void PreMethod(string name, params object[] arguments)
         {
             Console.WriteLine(string.Format("{0} Enter method: '{1}' Parameter: '{2}'", DateTime.Now, name, string.Join(", ", arguments)));
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
         }
         public void PostMethod(string name, params object[] arguments)
         {
-            Console.WriteLine(string.Format("{0} Leaving method: '{1}' Parameter: '{2}'", DateTime.Now, name, string.Join(", ", arguments)));
+            stopWatch.Stop();
+            Console.WriteLine(string.Format("{0} Leaving method: '{1}' Parameter: '{2}' Duration: '{3} ms'", DateTime.Now, name, string.Join(", ", arguments), stopWatch.ElapsedMilliseconds));
         }
     }
 }
