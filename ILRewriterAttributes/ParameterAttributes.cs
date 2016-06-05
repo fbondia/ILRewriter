1471ea4314125424085c2c86b635e3718d943616
Markus Schweitzer <me@markusschweitzer.at>
2016-06-05 20:52:29 +0200

    More parameter attributes.

--- ILRewriterAttributes/ParameterAttributes.cs
+++ ILRewriterAttributes/ParameterAttributes.cs
@@ -108,9 +108,8 @@ namespace ILRewriterAttributes
         }
     }
 
-
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    public class Email : Attribute
+    public class EmailValidator : Attribute
     {
         public static void Process(string methodName, string name, object value)
         {
@@ -126,4 +125,56 @@ namespace ILRewriterAttributes
             throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is not a valid E-Mail address.", methodName, name, value));
         }
     }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public class Base64Validator : Attribute
+    {
+        public static void Process(string methodName, string name, object value)
+        {
+            if (value != null)
+            {
+                string data = value.ToString();
+                if (IsBase64String(data))
+                {
+                    return;
+                }                
+            }
+
+            throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is not a valid base64 string.", methodName, name, value));
+        }
+
+        private static Char[] Base64Chars = new[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' };
+
+        public static bool IsBase64String(string value)
+        {
+            if (value == null || value.Length == 0 || value.Length % 4 != 0 || value.Contains(' ') || value.Contains('\t') || value.Contains('\r') || value.Contains('\n'))
+            {
+                return false;
+            }
+
+            var index = value.Length - 1;
+
+            if (value[index] == '=')
+            {
+                index--;
+            }
+
+            if (value[index] == '=')
+            {
+                index--;
+            }
+
+            for (var i = 0; i <= index; i++)
+            {
+                if (!Base64Chars.Contains(value[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+
 }
