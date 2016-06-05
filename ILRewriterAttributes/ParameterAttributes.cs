167ff2f5180c60799a9279af5e2f338e215d5250
Markus Schweitzer <me@markusschweitzer.at>
2016-06-05 20:43:58 +0200

    Added additional parameter attributes.

--- ILRewriterAttributes/ParameterAttributes.cs
+++ ILRewriterAttributes/ParameterAttributes.cs
@@ -37,6 +37,79 @@ namespace ILRewriterAttributes
     }
 
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public class GreaterThanZero : Attribute
+    {
+        public static void Process(string methodName, string name, object value)
+        {
+            if (value != null)
+            {
+                int data = Convert.ToInt32(value);
+                if (data <= 0)
+                {
+                    throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is not greater than zero.", methodName, name, value));
+                }
+            }
+
+            throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is null.", methodName, name, value));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public class GreaterOrEqualZero : Attribute
+    {
+        public static void Process(string methodName, string name, object value)
+        {
+            if (value != null)
+            {
+                int data = Convert.ToInt32(value);
+                if (data >= 0)
+                {
+                    throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is not greater or equal zero.", methodName, name, value));
+                }
+            }
+
+            throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is null.", methodName, name, value));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public class LesserThanZero : Attribute
+    {
+        public static void Process(string methodName, string name, object value)
+        {
+            if (value != null)
+            {
+                int data = Convert.ToInt32(value);
+                if (data >= 0)
+                {
+                    throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is not lesser than zero.", methodName, name, value));
+                }
+            }
+
+            throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is null.", methodName, name, value));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public class LesserOrEqualZero : Attribute
+    {
+        public static void Process(string methodName, string name, object value)
+        {
+            if (value != null)
+            {
+                int data = Convert.ToInt32(value);
+                if (data <= 0)
+                {
+                    throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is not lesser or equal zero.", methodName, name, value));
+                }
+            }
+
+            throw new ArgumentNullException(name, string.Format("Method '{0}' Parameter '{1}' Value '{2}' is null.", methodName, name, value));
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
     public class Email : Attribute
     {
         public static void Process(string methodName, string name, object value)
@@ -44,7 +117,7 @@ namespace ILRewriterAttributes
             if (value != null)
             {
                 string data = value.ToString();
-                if(System.Text.RegularExpressions.Regex.IsMatch(data, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
+                if (System.Text.RegularExpressions.Regex.IsMatch(data, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
                 {
                     return;
                 }
